6947e4444783eef5cf901728a4bbdb390f1c9009
scifaclint <scifaclint@gmail.com>
2022-09-27 17:36:50 -0700

    genging gimmick the code

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,29 +1,26 @@
-#include "main.h"
-
-/**
- * print_chessboard - prints the chess board
- *
- * @a: input pointer
- *
- * Return: return nothing
-*/
-
-void print_chessboard(char (*a)[8])
-{
-	int i, j;
-
-	i = 0;
-	j = 0;
-	while (i < 64)
-	{
-		if (i % 8 == 0 && i != 0)
-		{
-			j = i;
-			_putchar('\n');
-		}
-
-		_putchar(a[i / 8][i - j]);
-		i++;
-	}
-	_putchar('\n');
-}
+#include "main.h" 
+  
+ /** 
+  * print_chessboard - print the chessboard, 2d array 
+  * @a: 2d array of chars 
+  *Return: void 
+  */ 
+ void print_chessboard(char (*a)[8]) 
+ { 
+         /*Declaring variables*/ 
+         int i, j; 
+  
+         i = 0; 
+         while (i < 8) /*number repetitions*/ 
+         { 
+                 j = 0; 
+                 while (j < 8) 
+                 { 
+                         _putchar (a[i][j]); 
+                         j++; 
+                 } 
+                 _putchar ('\n'); /*print new line*/ 
+                 i++; /*add +1*/ 
+         } 
+  
+ }
